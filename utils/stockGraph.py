2644ced75928d0aeea9800549752369d197bd129
horikawa <horikawa.yoji.0156@colopl.co.jp>
2018-01-17 20:10:54 +0900

    added bolling graph

--- utils/stockGraph.py
+++ utils/stockGraph.py
@@ -11,17 +11,24 @@ stock = stss.StockDataFrame().retype(pd.read_csv("../3632.csv"))
 #各パラメータ初期化
 stock.get('macd')
 stock.get('rsi_6') #ワーニングが出るが気にしない
+stock.get('boll_ub') #ボリンジャー
+stock.get('boll_lb') #ボリンジャー
+
 
 openList = stock.as_matrix(columns=['open'])
 closeList = stock.as_matrix(columns=['close'])
 rsiList = stock.as_matrix(columns=['rsi_6'])
 macdList = stock.as_matrix(columns=['macd'])
 volumeList = stock.as_matrix(columns=['volume'])
+bollLBList = stock.as_matrix(columns=['boll_lb'])
+bollUBList = stock.as_matrix(columns=['boll_ub'])
 
 closeList = closeList[200:len(closeList)-1]
 rsiList = rsiList[200:len(rsiList)-1]
 macdList = macdList[200:len(macdList)-1]
 volumeList = volumeList[200:len(volumeList)-1]
+bollLBList = bollLBList[200:len(bollLBList)-1]
+bollUBList = bollUBList[200:len(bollUBList)-1]
 
 
 if __name__ == '__main__':
@@ -35,14 +42,15 @@ if __name__ == '__main__':
 
     #一つ目
     ax1 = fig.add_subplot(1,1,1)
-    ax1.set_ylabel('rsi')
-    p1, = ax1.plot(rsiList, label=r'rsi', color='blue')
+    ax1.set_ylabel('close')
+    p1, = ax1.plot(closeList, label=r'close', color='blue')
     # p2, = ax1.plot(openList)
 
     #2つ目
     ax2 = ax1.twinx()
-    ax2.set_ylabel('macd')
-    p2, = ax2.plot(macdList, label=r'macd', color='orange')
+    ax2.set_ylabel('boll')
+    p2, = ax2.plot(bollLBList, label=r'boll', color='orange')
+    p3, = ax2.plot(bollUBList, color='green')
 
     #3つ目
     # ax3 = fig.add_subplot(2,1,2)
@@ -50,6 +58,6 @@ if __name__ == '__main__':
 
 
     # plt.legend([p1, p2, p3], ["rsi", "macd", "close"])
-    plt.legend([p1, p2], ["rsi", "macd"])
+    plt.legend([p1, p2], ["close", "boll"])
 
     plt.show()
